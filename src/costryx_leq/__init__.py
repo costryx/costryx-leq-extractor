f8857e70d5dc644e2fdda61b27feaaabf9dd725c
0128 Team Commander & Chief Physical Architect <l24observer@gmail.com>
2026-05-26 03:07:09 +0800

    Update __init__.py

--- src/costryx_leq/__init__.py
+++ src/costryx_leq/__init__.py
@@ -7,10 +7,13 @@ from costryx_leq.uncertainty import sigma_leq, classify_gum_components, expanded
 from costryx_leq.mhl import compute_mhl_hash, verify_mhl_hash, MHLBundle
 from costryx_leq.visualization import plot_leq_spectrum_standard, COSTRYX_COLOR_GREEN, COSTRYX_COLOR_YELLOW, COSTRYX_COLOR_RED
 
-__version__ = "0.1.0-foundational"
+__version__ = "0.1.0"
+__variant__ = "foundational"
 __author__ = "Chin-Yu Hsu"
 __license__ = "Apache-2.0"
-__doi__ = "10.5281/zenodo.PENDING"
+__doi__ = "10.5281/zenodo.20382496"
+__concept_doi__ = "10.5281/zenodo.20382495"
+__paper_url__ = "https://doi.org/10.5281/zenodo.20382496"
 
 __all__ = [
     "extract_leq", "extract_leq_spectrum", "AnchorAssignment", "lambda_em",
@@ -19,4 +22,5 @@ __all__ = [
     "compute_mhl_hash", "verify_mhl_hash", "MHLBundle",
     "plot_leq_spectrum_standard", "COSTRYX_COLOR_GREEN", "COSTRYX_COLOR_YELLOW", "COSTRYX_COLOR_RED",
 ]
+
 
